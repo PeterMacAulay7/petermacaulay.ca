4acc521bf84d71f17dc130d2b9bd6bb926039705
Peter MacAulay <144722304+PeterMacAulay7@users.noreply.github.com>
2025-11-04 16:07:14 -0400

    Add files via upload

---
+++ imports/essays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Essays</title>
+  <link rel="icon" type="" href="PMsignature.png">
+  <link rel="stylesheet" href="style.css">
+</head>
+    <body>
+        <div>
+            More to come soon!
+        </div>
+    </body>
+</html>
---
+++ imports/right-sidebar.php
@@ -0,0 +1,26 @@
+<link rel="stylesheet" href="style.css">
+</div>
+      <div class = "sidebar">
+        <h2>
+            Status Update:
+          </h2>
+        <div class = "scroll-box">
+          <ul>
+            <li>Just updated the page to use the index.php file as a directory for all other pages. 
+              It took a lot longer than expected. &#40I guess thats to be expected&#41</li>
+            <li>Currently learning HTML/CSS/PHP</li>
+            <li>I have big plans for taking the dev side of this website to a new level;
+              you may not see all of it as a visitor, I will have a blog post or project update when I'm done.
+              Mostly because those are the types of pages that will be possible with my plans.
+            </li>
+          </ul>
+        </div>
+        <h2>
+          Next Improv Show:
+        </h2>
+        <div class = "scroll-box">
+          <ul>
+            <li>Wed, Nov 05 @8:30 in the pond</li>
+          </ul>
+        </div>
+    </div>
